b84480a08319b3e6a8750de67849bd4f50029883
CrissDev <crisshoang0711@gmail.com>
2025-03-25 11:37:15 +0700

    update

--- Backend_Recruiting_Apply_App/Program.cs
+++ Backend_Recruiting_Apply_App/Program.cs
@@ -1,25 +1,50 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using TopCVSystemAPIdotnet.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<RAADbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MyDB")));
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactNative", builder =>
+    {
+        builder.WithOrigins("http://192.168.137.230:8082")
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowCredentials();
+    });
+});
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5032); // HTTP
+    options.ListenAnyIP(7093, listenOptions =>
+    {
+        listenOptions.UseHttps(); // HTTPS với chứng chỉ mặc định
+    });
+});
+
+
+builder.Services.AddControllers();
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Sử dụng CORS
+app.UseCors("AllowReactNative");
+app.UseRouting();
 
 app.UseHttpsRedirection();
 
